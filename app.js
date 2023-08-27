785cd0b2f3c93a55c83691079788d4df872ed4c7
Muhammad Saqib Ilyas <msaqib@gmail.com>
2023-08-27 19:14:33 +0500

    Added the click handler

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
 const board = document.getElementById('board')
+const playerSpan = document.getElementById('player')
+let squares = null
+
+const columns = 7
+const rows = 6
+
+let turn = 0
+
+const player = ['red', 'blue']
+
+let countColumns = [0, 0, 0, 0, 0, 0, 0]
+
 function initializeBoard() {
     for (let i = 0 ; i < 7 ; i++) {
         for (let j = 0 ; j < 6 ; j++) {
@@ -6,6 +18,17 @@ function initializeBoard() {
             board.appendChild(div)
         }
     }
+    squares = board.querySelectorAll(":scope > div")
+    squares.forEach( (square, index) => {
+        square.onclick = () => {
+            turn = 1 - turn
+            playerSpan.innerHTML = turn + 1
+        }
+    })
+}
+
+function getColumnTopIndex(divindex) {
+    return divindex % columns 
 }
 
 initializeBoard()
